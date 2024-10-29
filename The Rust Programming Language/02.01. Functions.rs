5b29d931e24f5c1c0f2cbaf3d0a1b90b563b5693
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2024-10-29 11:42:20 +0100

    Update 02.01. Functions.rs

--- The Rust Programming Language/02.01. Functions.rs
+++ The Rust Programming Language/02.01. Functions.rs
@@ -1,1 +1,24 @@
+fn main() {
+    let mut my_nickname = "Theo";
+    greet(my_nickname);
+    goodbye(my_nickname);
 
+    my_nickname = "oTTeuM";
+    greet(my_nickname);
+    goodbye(my_nickname);
+
+    my_nickname = "truenjenir";
+    greet(my_nickname);
+    goodbye(my_nickname);
+
+    my_nickname = "Teo Goli";
+    greet(my_nickname);
+    goodbye(my_nickname);
+}
+fn greet(my_nickname: &str) {
+    println!("{my_nickname}! I greet you.");
+    println!("Welcome to the world of Rust!");
+}
+fn goodbye(my_nickname: &str) {
+    println!("Goodbye {my_nickname}!");
+}
