0d43dec1fb1e3f70609a449a28e9b2174cee6613
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2024-10-29 11:18:01 +0100

    Create 01.1. Getting ready.rs

---
+++ The Rust Programming Language/01.1. Getting ready.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut ime_priimek = "Teo Goli";
+    println!("Yo, {ime_priimek}!");
+
+    ime_priimek = "oTTeuM";
+    println!("Kwa bti rad, {ime_priimek}");
+
+    ime_priimek = "truenjenir";
+    println!("Kje si, {ime_priimek}");
+
+    ime_priimek = "Teo";
+    println!("Pravš, {ime_priimek}");
+}
