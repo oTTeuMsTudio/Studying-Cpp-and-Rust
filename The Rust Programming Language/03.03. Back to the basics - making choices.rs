4ea9c6d07d5851e2a0e11e0be41be2642a09ee40
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2024-11-01 06:18:29 +0100

    Update 03.03. Back to the basics - making choices.rs

--- The Rust Programming Language/03.03. Back to the basics - making choices.rs
+++ The Rust Programming Language/03.03. Back to the basics - making choices.rs
@@ -1,1 +1,20 @@
+// Back to the basics - making choices
 
+fn main() {
+    println!("What is your name?");
+    let input = read_string();
+    if input == "" {
+        println!("You did not enter your name...");
+    } else {
+        println!("Your name is: {input}");
+    }
+}
+
+fn read_string() -> String {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("can not read user input");
+    let cleaned_input = input.trim().to_string();
+    cleaned_input
+}
